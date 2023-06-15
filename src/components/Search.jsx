3eb32ae2c28d2eecfdf7b5139cd11ec6e5ce801c
Ayush Mishra <105230070+reck-coder123@users.noreply.github.com>
2023-06-15 20:07:25 +0530

    Update Search.jsx

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -35,47 +35,55 @@ function Search() {
   }
   return (
     <>
-    {isDesktopOrLaptop && <div className='search relative h-[65rem] px-[5rem] bg-[#081730] pt-[18rem] pb-[10rem]
-    mt-[-15rem] z-[1] flex items-center justify-between rounded-b-[5rem] '>
-      {/*left side */}
-      <div className="left flex-1 z-[-1] relative">
+    {isDesktopOrLaptop && <div className="search relative h-[65rem] px-[5rem] bg-[#081730] pt-[18rem] pb-[10rem] mt-[-15rem] z-[1] flex items-center justify-between rounded-b-[5rem]">
+      {/* left side */}
+      <div className="left flex-1 z-[-1]">
         <motion.img
           variants={bg}
           animate={`${elementIsVisible}`}
           transition={{
             duration: 1,
-            type: "ease-out"
-          }} src={require("../img/backgraphics.png")} alt="" className='absolute top-[22rem] left-[-47rem]'
+            type: "ease-out",
+          }}
+          src={require("../img/backgraphics.png")}
+          alt=""
+          className="absolute top-[22rem] left-[-47rem]"
         />
-        <motion.img src={require("../img/d1.png")}
-
+        <motion.img
+          src={require("../img/d1.png")}
           alt=""
-          className="w-[16rem] top-[26rem] absolute" />{" "}
-        <motion.img src={require("../img/d2.png")}
+          className="w-[16rem] top-[26rem] absolute"
+        />{" "}
+        <motion.img
+          src={require("../img/d2.png")}
           alt=""
-          className="w-[9rem] absolute top-[32.7rem] left-[7rem]" />{" "}
-        <motion.img src={require("../img/d3.png")}
+          className="w-[9rem] absolute top-[32.7rem] left-[7rem]"
+        />{" "}
+        <motion.img
           variants={redimg}
           animate={`${elementIsVisible}`}
           transition={{
             duration: 1.2,
             type: "ease-out",
           }}
+          src={require("../img/d3.png")}
           alt=""
-          className="w-[9rem] top-[33rem] left-[17rem] absolute" />
-        <motion.img src={require("../img/d4.png")}
+          className="w-[9rem] top-[33rem] left-[17rem] absolute"
+        />
+        <motion.img
           variants={musicimg}
           animate={`${elementIsVisible}`}
           transition={{
             duration: 1,
             type: "ease-out",
           }}
+          src={require("../img/d4.png")}
           alt=""
-          className="w-[17rem] top-[50rem] left-[2rem] absolute" />
+          className="w-[17rem] top-[50rem] left-[2rem] absolute"
+        />
       </div>
-      {/*right side */}
+      {/* right side */}
       <div className="right flex items-start flex-col justify-start flex-1 h-[100%] pt-[9rem]">
-
         {/* Search */}
         <div className="searchbar flex justify-start w-[100%]">
           <input
@@ -101,8 +109,7 @@ function Search() {
           />
         </div>
 
-        {/*paragraph*/}
-
+        {/* paragraph */}
         <div className="detail flex flex-col mt-5 text-4xl">
           <span>Search Music by</span>
           <span>
@@ -113,13 +120,11 @@ function Search() {
             eget. Vestibulum ullamcorper <br /> volutpat varius.
           </span>
         </div>
-
-        {/*Music Player */}
+        {/* Music Player */}
         <VisibilitySensor
-        onChange={(isVisible)=>setElementIsVisible(isVisible)}
+          onChange={(isVisible) => setElementIsVisible(isVisible)}
         >
-
-        <MusicPlayer />
+          <MusicPlayer />
         </VisibilitySensor>
       </div>
     </div>}
